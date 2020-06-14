cae54a6bbaedb87040c2c82ab62cdcf6a4849919
arsonal_xu <31698025+arsodroid@users.noreply.github.com>
2020-06-14 23:22:47 +0800

    Docker部署

--- src/main/java/hello/dao/BlogDao.java
+++ src/main/java/hello/dao/BlogDao.java
@@ -2,15 +2,34 @@ package hello.dao;
 
 import hello.entity.Blog;
 import hello.entity.BlogResult;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Service
 public class BlogDao {
+    private final SqlSession sqlSession;
+
+    @Inject
+    public BlogDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
     public List<Blog> getBlogs(Integer page, Integer pageSize, Long userId, Boolean atIndex) {
-        return null;
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("userId", userId);
+        parameters.put("offset", (page - 1) * pageSize);
+        parameters.put("limit", pageSize);
+        return this.sqlSession.selectList("selectBlog", parameters);
     }
 
     public int count(Long userId) {
-        return 0;
+        int countBlog = sqlSession.selectOne("countBlog", userId);
+        return countBlog;
     }
 }
--- src/test/java/hello/integration/MyIntegrationTest.java
+++ src/test/java/hello/integration/MyIntegrationTest.java
@@ -1,18 +1,48 @@
 package hello.integration;
 
 import hello.Application;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
+//@ComponentScan("hello.*")
 public class MyIntegrationTest {
+    @Inject
+    Environment environment;
     @Test
-    void myIntegrationTest() {
+    void myIntegrationTest() throws IOException {
+        String port = environment.getProperty("local.server.port");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:" + port + "/auth");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
 
+        try {
+            Assertions.assertEquals(response.getStatusLine().getStatusCode(), 200);
+            HttpEntity entity = response.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            Assertions.assertTrue(EntityUtils.toString(entity).contains("用户没有登录"));
+
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
     }
 }
