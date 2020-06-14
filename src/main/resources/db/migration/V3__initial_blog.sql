c4b571d3cecd2e39f673d8004f1c2b1cc8f46936
arsonal_xu <31698025+arsodroid@users.noreply.github.com>
2020-06-15 00:37:52 +0800

    D:/Program Files/Git/blog 接口添加User信息

--- src/main/resources/db/migration/V3__initial_blog.sql
+++ src/main/resources/db/migration/V3__initial_blog.sql
@@ -1,10 +1,10 @@
 insert into `blog`(
-user_id ,
-title ,
-description ,
-content ,
-created_at ,
-updated_at
+    user_id ,
+    title ,
+    description ,
+    content ,
+    created_at ,
+    updated_at
 ) values (1, "title1", "description1", "content1", now(), now()),
          (2, "title2", "description2", "content2", now(), now()),
          (3, "title3", "description3", "content3", now(), now());
